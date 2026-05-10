5c5e04a51399b020000c672b599afb1cf0347b3e
marklvov <marklvov@altmail.kr>
2026-05-10 17:37:45 +0200

    games bottom padding

--- app/[locale]/(main)/slots/components/slots-server.tsx
+++ app/[locale]/(main)/slots/components/slots-server.tsx
@@ -3,7 +3,7 @@ import SlotsCarousel from './slots-main-carousel'
 
 export default async function SlotsServer() {
   return (
-    <div className="mt-5 space-y-2">
+    <div className="mt-5 space-y-2 pb-[70px] lg:pb-0">
       <SlotsHeader />
       <SlotsCarousel />
     </div>
